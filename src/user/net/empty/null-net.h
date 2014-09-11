29c17ca98c6aced3310a16ee6ecad8f76bdf3326
Peter Ruckebusch <peter.ruckebusch@intec.ugent.be>
2014-09-11 14:00:07 +0200

    Added rime stack, Makefile.net

---
+++ src/user/net/empty/null-net.h
@@ -0,0 +1,13 @@
+/**
+ * \file
+ *         An empty network stack implementation that does not do anything.
+ * \author
+ *         Adam Dunkels <adam@sics.se>
+ */
+
+#ifndef __NULL_NET_H__
+#define __NULL_NET_H__
+
+extern const struct network_driver nullnet_driver;
+
+#endif /* __NULL_NET_H__ */

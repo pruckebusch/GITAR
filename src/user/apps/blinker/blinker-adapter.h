b076b9a144d51ec9df842a03cae287602c06e0cd
Peter Ruckebusch <peter.ruckebusch@intec.ugent.be>
2014-10-06 12:12:50 +0200

    Changed blinker application

--- src/user/apps/blinker/blinker-adapter.h
+++ src/user/apps/blinker/blinker-adapter.h
@@ -3,31 +3,35 @@
 
 #include "kernel.h"
 
-static void blinker_adapter_init();
+void adapter_init();
 
 static component_id blinker_comp_id = 10;
-static const component_t blinker_comp = {{blinker_comp_id, 1, 0, APP_COMPONENT, 7, "blinker"}, {NULL, NULL, 0, 0}, {&blinker_adapter_init, NULL, NULL}} ;
+static const component_t blinker_comp = {{10, 1, 0, APP_COMPONENT, 7, "blinker"}, {NULL, NULL, 0, 0}, {&adapter_init, NULL, NULL}} ;
 
-static component_user_list_entry_t process_user = {NULL, blinker_comp_id};
+/*static component_user_list_entry_t process_user = {NULL, blinker_comp_id};
 static component_user_list_entry_t leds_user = {NULL, blinker_comp_id};
 static component_user_list_entry_t etimer_user = {NULL, blinker_comp_id};
-
-static void blinker_adapter_init(){
+*/
+void adapter_init(){
 
 	//Add component to gitar	
-	blinker_comp_id = kernel_add_cmp(&blinker_comp);
-
+	//~ blinker_comp_id = kernel_add_cmp(&blinker_comp);
+	blinker_comp_id = a(&blinker_comp);
+	
 	//process_user.user_id = blinker_comp_id;
 	process_user_adapter_init();
-	kernel_add_cmp_user(&process_user, process_cmp_local_id );
+	//kernel_add_cmp_user(&process_user, process_cmp_local_id );
 	
 	//leds_user.user_id = blinker_comp_id;
 	leds_user_adapter_init();
-	kernel_add_cmp_user(&leds_user, leds_cmp_local_id );
+	//kernel_add_cmp_user(&leds_user, leds_cmp_local_id );
 	
 	//etimer_user.user_id = blinker_comp_id;
 	etimer_user_adapter_init();
-	kernel_add_cmp_user(&etimer_user, etimer_cmp_local_id );
+	//kernel_add_cmp_user(&etimer_user, etimer_cmp_local_id );
+	
+	autostart_user_adapter_init();
+	
 }
 
 #endif /* __BLINKER_ADAPTER_H__ */
--- src/user/apps/blinker/blinker.c
+++ src/user/apps/blinker/blinker.c
@@ -1,10 +1,17 @@
 #include "contiki-conf.h"
-#include <stdio.h>
 
-#include "include/process.h"
-#include "include/leds.h"
-#include "include/etimer.h"
-#include "autostart.h"
+#include "include/system/hil/sys/process/process.h"
+#include "include/system/hil/dev/leds.h"
+#include "include/system/hil/sys/timer/etimer.h"
+#include "include/system/hil/sys/process/autostart.h"
+
+#define DEBUG 0
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
 
 /*---------------------------------------------------------------------------*/
 PROCESS(test_blink, "Test blink process");
@@ -18,11 +25,11 @@ PROCESS_THREAD(test_blink, ev, data)
 	static struct etimer t;
 	PROCESS_BEGIN();
 	
-	blinker_adapter_init();
+	//blinker_adapter_init();
 	
 	etimer_set(&t, CLOCK_SECOND);
 	while(1) {
-		//printf("Blinker started, ledOn ledOff\n");		
+		PRINTF("Blinker started, ledOn ledOff\n");		
 		leds_on(LEDS_GREEN);
 		PROCESS_WAIT_UNTIL(etimer_expired(&t));
 		etimer_reset(&t);
